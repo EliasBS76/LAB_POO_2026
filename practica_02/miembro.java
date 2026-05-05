ee4502ac0706d30787ef0c3a6feadbc2ed8e90ab
EliasBs76 <eliasbernal123@gmail.com>
2026-05-05 11:37:25 -0600

    Creacion de los 5 metodos de instancia

--- practica_02/miembro.java
+++ practica_02/miembro.java
@@ -9,7 +9,7 @@ public class miembro
     public String Meses_inscrito;
     public boolean inscrito;
 
-    //Informacion completa del miembro
+    //Informacion total
     public miembro (String nombre , int telefono,int id , String Tipo_Plan , String Meses_inscrito ,boolean inscrito)
     {
         this.nombre = nombre;
@@ -20,7 +20,7 @@ public class miembro
         this.inscrito = inscrito;
     }
 
-    //Status del miembro
+    //Activos
     public miembro (String nombre , int id ,boolean inscrito)
     {
         this.nombre = nombre;
@@ -28,12 +28,50 @@ public class miembro
         this.inscrito = inscrito;
     }
 
-    //Peticion de informacion
+    //Peticion de información
     public miembro (String nombre , int telefono)
     {
         this.nombre = nombre;
         this.telefono = telefono;
     }
+
+    public void info()
+    {
+        System.out.println("ID DE USUARIO " + id + "Nombre : " + nombre + "Plan Actual : " + Tipo_Plan);
+    }
+
+    public void actualizar_Insicripcion()
+    {
+        this.inscrito = true;
+        System.out.println("Se renovo la suscripcion con exito : " +nombre);
+
+    }
+
+    public void Dar_De_Baja()
+    {
+        this.inscrito = false;
+        System.out.println("La suscripcion fue cancelada con exito : " + nombre);
+    }
+
+    public void actualizar_Info(String nombre , int telefono , String Tipo_Plan)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.Tipo_Plan = Tipo_Plan;
+        
+    }
+
+    public void actualizar(int telefono)
+    {
+        this.telefono = telefono;
+    }
+
+    public void actualizar(String Tipo_Plan)
+    {
+        this.Tipo_Plan = Tipo_Plan;
+    }
+
+
     
 }
 
